fa9607628e320dcc44f9edfffacf154c784fc0a3
James Baicoianu <james_github@baicoianu.com>
2017-01-22 00:01:23 -0800

    Return promise from share function, inherit from base

--- scripts/targets/file.js
+++ scripts/targets/file.js
@@ -13,23 +13,32 @@ elation.require(["ui.base"], function() {
       return (this.types.indexOf('*') != -1 || this.types.indexOf(type) != -1);
     }
     this.share = function(data, type) {
-      console.log('share a file', data, type);
-      var a = document.createElement('A');
-      a.style.display = 'none';
-      document.body.appendChild(a);
+      return new Promise(elation.bind(this, function(resolve, reject) {
+        console.log('share a file', data, type);
+        var a = document.createElement('A');
+        a.style.display = 'none';
+        document.body.appendChild(a);
 
-      var imgdata = data.image;
-      if (imgdata instanceof Uint8Array) {
-        imgdata = new Blob([imgdata.buffer], {type: data.type});
-      }
+        var imgdata = data.image;
+        if (imgdata instanceof Uint8Array) {
+          imgdata = new Blob([imgdata.buffer], {type: data.type});
+        }
 
-      var url = window.URL.createObjectURL(imgdata);
+        var url = window.URL.createObjectURL(imgdata);
 
-      a.href = url;
-      a.download = data.name;
-      a.click();
-      window.URL.revokeObjectURL(url);
+        a.href = url;
+        a.download = data.name;
+        a.click();
+        window.URL.revokeObjectURL(url);
+
+        var upload = elation.share.upload({data: data, requests: false, append: this, target: this});
+
+        setTimeout(elation.bind(this, function(upload) { upload.addclass('state_removing'); }, upload), 4000);
+        setTimeout(elation.bind(this, function(upload) { upload.reparent(false); this.refresh(); elation.events.fire({element: this, type: 'content_hide'}); }, upload), 6000);
+
+        resolve(upload);
+      }));
     }
-  }, elation.ui.base);
+  }, elation.share.targets.base);
 });
 
